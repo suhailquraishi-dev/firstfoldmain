4d9079f8bea3fd8cda800927ba8b3c79cb8c960a
Suhail Quraishi <suhailquraishi@Suhails-MacBook-Air.local>
2026-08-01 23:46:10 +0530

    Fix Vercel static deployment

---
+++ cloudflare-workers.d.ts
@@ -0,0 +1,15 @@
+declare module "cloudflare:workers" {
+  export const env: {
+    DB?: D1Database;
+  };
+}
+
+interface Fetcher {
+  fetch(input: RequestInfo | URL, init?: RequestInit): Promise<Response>;
+}
+
+interface D1Database {
+  prepare(query: string): unknown;
+  batch<T = unknown>(statements: unknown[]): Promise<T[]>;
+  exec(query: string): Promise<unknown>;
+}
